138ddcd7fc03e4b77f2ba6bf96b56dd6837cf511
Kyle Barton <kyle.humberto@gmail.com>
2024-02-12 09:53:56 -0800

    Add csv import service

---
+++ app/models/csv_import_service.rb
@@ -0,0 +1,13 @@
+class CsvImportService
+  require 'csv'
+
+  def initialize(file)
+    @file = file
+  end
+
+  def call
+    CSV.foreach(@file.path, headers: true) do |row|
+      Contact.upsert(row.to_hash)
+    end
+  end
+end
