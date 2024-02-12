f1bcec6c7be16e19db1aa3a8aeac18a6c7f4a714
Kyle Barton <kyle.humberto@gmail.com>
2024-02-12 09:41:47 -0800

    Implement contact resource

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Contact do
 
+ActiveAdmin.register Contact do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -9,10 +10,26 @@ ActiveAdmin.register Contact do
   #
   # or
   #
-  # permit_params do
-  #   permitted = [:name, :email, :address, :city, :state, :zip]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params do
+    permitted = [:name, :email, :address, :city, :state, :zip]
+    # permitted << :other if params[:action] == 'create' && current_user.admin?
+    permitted
+  end
+
+  collection_action :upload_csv, method: :post do
+    # Logic to parse and process CSV file
+    CsvProcessor.new(params[:csv_file]).process
+    redirect_to collection_path, notice: "Contacts have been uploaded."
+  end
+
+  action_item :upload_csv, only: :index do
+    link_to 'Upload CSV', action: 'upload_csv'
+  end
+
+  # Form to upload CSV
+  collection_action :upload_csv, method: :get do
+    render "admin/csv/upload_csv"
+  end
+end
+
 end
