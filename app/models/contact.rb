9aa3310fb563a986d4fa2e805495db6759db8fb0
Kyle Barton <kyle.humberto@gmail.com>
2024-02-14 12:30:41 -0800

    Explicitly set ransackable attributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["address", "city", "created_at", "email", "id", "id_value", "name", "state", "updated_at", "zip"]
+  end
 end
--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -1,2 +1,5 @@
 class EmailTemplate < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["body", "created_at", "id", "id_value", "name", "subject", "updated_at"]
+  end
 end
