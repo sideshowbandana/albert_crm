b5a4b08ca4f5b601cdc50dd54c0b9f3d1aaa932d
Kyle Barton <kyle.humberto@gmail.com>
2024-02-14 12:30:28 -0800

    Add email receipt resource

---
+++ app/admin/email_receipt.rb
@@ -0,0 +1,32 @@
+ActiveAdmin.register EmailReceipt do
+  # Customizing the index table
+  index do
+    selectable_column
+    id_column
+    column :contact_id
+    column :email_template_id
+    column :status
+    column :receipt_number
+    column :created_at
+    actions
+  end
+
+  # Filters
+  filter :contact_id
+  filter :email_template_id
+  filter :status
+  filter :receipt_number
+  filter :created_at
+
+  # Form customization (if necessary)
+  form do |f|
+    f.inputs do
+      f.input :status
+      f.input :receipt_number
+    end
+    f.actions
+  end
+
+  # Permitting parameters
+  permit_params :status, :receipt_number
+end
--- app/models/email_receipt.rb
+++ app/models/email_receipt.rb
@@ -1,4 +1,8 @@
 class EmailReceipt < ApplicationRecord
   belongs_to :contact
   belongs_to :email_template
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["contact_id", "created_at", "email_template_id", "id", "id_value", "receipt_number", "status", "updated_at"]
+  end
 end
