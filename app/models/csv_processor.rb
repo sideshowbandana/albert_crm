a6a846f4510129eb95c1554c5afb59419809d0c6
Kyle Barton <kyle.humberto@gmail.com>
2024-02-14 20:46:39 -0800

    Update the csv processor to handle csv without headers
    The sample input doesn't have headers so I'm expecting any tests to
    not have headers as well.

--- app/models/csv_processor.rb
+++ app/models/csv_processor.rb
@@ -1,13 +1,20 @@
 class CsvProcessor
   require 'csv'
 
+  HEADERS=[:name, :email, :phone_number, :address, :city, :state, :zip]
+
   def initialize(file)
     @file = file
   end
 
-  def call
-    CSV.foreach(@file.path, headers: true) do |row|
-      Contact.upsert(row.to_hash)
+  def process
+    data = {  }
+    CSV.foreach(@file.path) do |row|
+      attributes = Hash[HEADERS.zip(row)]
+      # if the csv has duplicates, take the last one.
+      data[attributes[:email]] = attributes
     end
+    # if we have a duplicate, update it.
+    Contact.upsert_all(data.values, unique_by: :email, on_duplicate: :update)
   end
 end
