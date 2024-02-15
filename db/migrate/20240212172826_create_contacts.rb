00775f8f8dbd4edf51fb65c95722513a621fb2f9
Kyle Barton <kyle.humberto@gmail.com>
2024-02-14 20:48:46 -0800

    Add necessary indexes for contacts

--- db/migrate/20240212172826_create_contacts.rb
+++ db/migrate/20240212172826_create_contacts.rb
@@ -10,5 +10,11 @@ class CreateContacts < ActiveRecord::Migration[7.1]
 
       t.timestamps
     end
+    add_index :contacts, :email, unique: true
+    add_index :contacts, :address
+    add_index :contacts, :name
+    add_index :contacts, :city
+    add_index :contacts, :state
+    add_index :contacts, :zip
   end
 end
