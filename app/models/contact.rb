21e875881200e7889a05ac13edc71617cf603b78
Kyle Barton <kyle.humberto@gmail.com>
2024-02-16 09:24:03 -0800

    Implement contact model spec
    Add shoulda matchers to verify downstream actions like dependent
    destroy

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ApplicationRecord
-  has_many :email_receipts
-  validates_presence_of :email
+  has_many :email_receipts, dependent: :destroy
+  validates_presence_of :email, :name
+  validates_uniqueness_of :email
 
   def self.ransackable_attributes(auth_object = nil)
     ["address", "city", "created_at", "email", "id", "id_value", "name", "state", "phone_number", "updated_at", "zip"]
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Validations
+  describe 'validations' do
+    it 'is valid with valid attributes' do
+      contact = FactoryBot.build(:contact)
+      expect(contact).to be_valid
+    end
+
+    it 'is not valid without a name' do
+      contact = FactoryBot.build(:contact, name: nil)
+      expect(contact).not_to be_valid
+    end
+
+    it 'is not valid without an email' do
+      contact = FactoryBot.build(:contact, email: nil)
+      expect(contact).not_to be_valid
+    end
+
+    it 'is not valid without a unique email' do
+      FactoryBot.create(:contact, email: 'test@example.com')
+      contact = FactoryBot.build(:contact, email: 'test@example.com')
+      expect(contact).not_to be_valid
+    end
+  end
+
+  # Associations
+  describe 'associations' do
+    it { should have_many(:email_receipts).dependent(:destroy) }
+  end
 end
