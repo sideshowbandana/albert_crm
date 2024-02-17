c5da1a95041e92d7131bf629ddaead1b5f82c8f4
Kyle Barton <kyle.humberto@gmail.com>
2024-02-16 21:10:42 -0800

    Use netcat to determine if postgres is up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,13 @@
 #!/bin/bash -e
 
-set -x
-
-retry_count=0
-max_retries=2
-
-# Function to check database connection and prepare it
-prepare_database() {
-    if ! ./bin/rails db:prepare; then
-        echo "Failed to connect to the database."
-        return 1
-    fi
-}
-
 # If running the rails server, then attempt to prepare the database
 if [ "${1}" == "./bin/rails" ] && [ "${2}" == "server" ]; then
-    until prepare_database; do
-        ((retry_count++))
-        echo "Attempting to reconnect to the database. Attempt: ${retry_count}"
-        if [ "$retry_count" -ge "$max_retries" ]; then
-            echo "Failed to connect to the database after ${max_retries} attempts."
-            exit 1
-        fi
-        sleep 5 # Wait for 5 seconds before retrying
+    while ! nc -z db 5432; do
+        echo "Waiting for database"
+        sleep 1 # wait for 1 second before check again
     done
-else
-    echo $1
-    echo $2
+
+    ./bin/rails db:prepare
 fi
 
 exec "${@}"
